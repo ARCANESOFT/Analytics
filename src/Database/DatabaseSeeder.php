95797006f59be8263f2ae7365f724275bef8b455
ARCANEDEV <arcanedev.maroc@gmail.com>
2021-06-09 11:22:14 +0100

    first commit

---
+++ src/Database/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arcanesoft\Analytics\Database;
+
+use Arcanesoft\Foundation\Support\Database\Seeder;
+
+/**
+ * Class     DatabaseSeeder
+ *
+ * @package  Arcanesoft\Analytics\Database
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class DatabaseSeeder extends Seeder
+{
+    /* -----------------------------------------------------------------
+     |  Main Methods
+     | -----------------------------------------------------------------
+     */
+
+    /**
+     * Get the seeders.
+     *
+     * @return array
+     */
+    public function seeders(): array
+    {
+        return [];
+    }
+}
